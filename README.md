84f1238d61275a2045e85871616cacccaf672d20
Dustin Vannatter <dustin@dustin.local>
2014-01-22 15:13:58 -0500

    init

---
+++ README.md
@@ -0,0 +1,35 @@
+# CorgiCoin [CORG, Ç]
+http://corgicoin.com/
+
+![CorgiCoin](http://static.tumblr.com/ppdj5y9/Ae9mxmxtp/300coin.png)
+
+## What is CorgiCoin?
+CorgiCoin is like Bitcoin, but based on Litecoin, and very short.
+http://dogecoin.com/
+
+## License
+CorgiCoin is released under the MIT license. See [COPYING](COPYING)
+for more information.
+
+## FAQ
+
+### Who are you?
+Just a couple guys having fun.
+
+### What's the max CORG that will exist?
+Only 10 billion. Wait, what.
+
+### Coin type?
+CorgiCoin is a Scrypt-based Proof of Work coin.
+
+1 Minute Block Targets, 4 Hour Diff Readjustments
+
+Special reward system: Random block rewards
+
+1-100,000: 0-1,000,000 Dogecoin Reward
+100,001 — 200,000: 0-500,000 Dogecoin Reward
+200,001 — 300,000: 0-250,000 Dogecoin Reward
+300,001 — 400,000: 0-125,000 Dogecoin Reward
+400,001 — 500,000: 0-62,500 Dogecoin Reward
+500,001 - 600,000: 0-31,250 Dogecoin Reward
+600,000+ — 10,000 Reward (flat)
---
+++ src/checkpoints.cpp
@@ -0,0 +1,67 @@
+// Copyright (c) 2009-2012 The Bitcoin developers
+// Copyright (c) 2011-2012 Litecoin Developers
+// Copyright (c) 2013 CorgiCoin Developers
+// Distributed under the MIT/X11 software license, see the accompanying
+// file COPYING or http://www.opensource.org/licenses/mit-license.php.
+
+#include <boost/assign/list_of.hpp> // for 'map_list_of()'
+#include <boost/foreach.hpp>
+
+#include "checkpoints.h"
+
+#include "main.h"
+#include "uint256.h"
+
+namespace Checkpoints
+{
+    typedef std::map<int, uint256> MapCheckpoints;
+
+    //
+    // What makes a good checkpoint block?
+    // + Is surrounded by blocks with reasonable timestamps
+    //   (no blocks before with a timestamp after, none after with
+    //    timestamp before)
+    // + Contains no strange transactions
+    //
+
+    static MapCheckpoints mapCheckpoints =
+            boost::assign::map_list_of
+            (  0, hashGenesisBlockOfficial )
+            (  42279, uint256("0x8444c3ef39a46222e87584ef956ad2c9ef401578bd8b51e8e4b9a86ec3134d3a"))
+            (  42400, uint256("0x557bb7c17ed9e6d4a6f9361cfddf7c1fc0bdc394af7019167442b41f507252b4"))
+			;
+
+
+    bool CheckBlock(int nHeight, const uint256& hash)
+    {
+        if (fTestNet) return true; // Testnet has no checkpoints
+
+        MapCheckpoints::const_iterator i = mapCheckpoints.find(nHeight);
+        if (i == mapCheckpoints.end()) return true;
+        return hash == i->second;
+		// return true;
+    }
+
+    int GetTotalBlocksEstimate()
+    {
+        if (fTestNet) return 0;
+	
+        return mapCheckpoints.rbegin()->first;
+		// return 0;
+    }
+
+    CBlockIndex* GetLastCheckpoint(const std::map<uint256, CBlockIndex*>& mapBlockIndex)
+    {
+        if (fTestNet) return NULL;
+
+        BOOST_REVERSE_FOREACH(const MapCheckpoints::value_type& i, mapCheckpoints)
+        {
+            const uint256& hash = i.second;
+            std::map<uint256, CBlockIndex*>::const_iterator t = mapBlockIndex.find(hash);
+            if (t != mapBlockIndex.end())
+                return t->second;
+				// return NULL;
+        }
+        return NULL;
+    }
+}
